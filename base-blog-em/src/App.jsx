51b453acc797f52dc13a5ef4c13f333d152f2dd2
su-eun <58408937+su-eun@users.noreply.github.com>
2024-03-26 22:03:15 +0900

    first commit

---
+++ base-blog-em/src/App.jsx
@@ -0,0 +1,19 @@
+import { Posts } from "./Posts";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog &apos;em Ipsum</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
